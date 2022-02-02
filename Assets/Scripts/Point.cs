5a9c1dd7bdaa0296dde547864a5f1ea57c9e06e3
Dchan <ghost0910@mail.ru>
2022-02-02 23:44:38 +0300

    create a maze

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    public GameObject leftWall;
+    public GameObject bottomWall;
+    public GameObject floor;
+    public bool isFinishPoint = false;
+    public int indexForRoute;
+}
